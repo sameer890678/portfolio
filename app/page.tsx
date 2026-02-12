d506bdcd30b93df968c201166e36d06bd69d0d92
sameer_first <maliksameer890678@gmail.com>
2026-02-12 20:08:22 +0500

    Chnaged hero section position

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,8 @@ export default function Home() {
   }, [messageIndex]);
 
   return (
-    <div className="bg-blue text-white">
+    <div className="flex items-center justify-center min-h-screen bg-black text-white">
+
       {/* Hero Section */}
       <section className="flex flex-col items-center justify-center min-h-screen px-4">
         <h1 className="text-5xl md:text-6xl font-extrabold text-center bg-clip-text text-transparent bg-gradient-to-r from-purple-600 via-pink-600 to-yellow-600">
