77482347d9c327eacdd23cc6909030eb20b850bf
sameer_first <maliksameer890678@gmail.com>
2026-02-16 20:17:35 +0500

    redoing changes

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
   <div className="grid grid-cols-1 md:grid-cols-2 gap-20 justify-items-center">
     
    {/* Tic Tac Toe Project */}
-<div className="p-6 rounded-lg text-white shadow-lg tictactoe-hover max-w-sm hover:scale-105 transition-transform duration-[3000]">
+<div className="p-6 rounded-lg text-white shadow-lg tictactoe-hover max-w-sm hover:scale-105 transition-transform duration-500">
   <h3 className="text-2xl font-semibold mb-2">Tic Tac Toe Game</h3>
   <p>
     Built a 2-player Tic Tac Toe game in C++. Learned about arrays, loops, functions, and game logic implementation. Applied programming concepts to create an interactive console game.
@@ -73,7 +73,7 @@ export default function Home() {
 </div>
 
     {/* Army Management Project */}
-<div className="p-6 rounded-lg text-white cursor-pointer shadow-lg army-hover max-w-sm hover:scale-105 transition-transform duration-500">
+<div className="p-6 rounded-lg text-white shadow-lg army-hover max-w-sm hover:scale-105 transition-transform duration-500">
   <h3 className="text-2xl font-semibold mb-2">Army Management System</h3>
   <p>
     Developed a console-based Army Management System in C++. Managed soldiers, weapons, and resources using data structures and file handling. Learned object-oriented programming concepts and organized complex data efficiently.
