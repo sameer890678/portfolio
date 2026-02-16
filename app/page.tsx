306daf32de701de5267e667b6ab8a4b3b6940694
sameer_first <maliksameer890678@gmail.com>
2026-02-16 17:08:02 +0500

    removing flex

--- app/page.tsx
+++ app/page.tsx
@@ -36,9 +36,9 @@ export default function Home() {
   }, [messageIndex]);
 
   return (
-    <div className="bg-white text-white">
+    <div className="bg-black text-white">
       {/* Hero Section */}
-      <section className="flex flex-col items-center justify-center min-h-screen px-4">
+      <section className="flex-col items-center justify-center min-h-screen px-4">
         <h1 className="text-5xl md:text-6xl font-extrabold text-center bg-clip-text text-transparent bg-gradient-to-r from-purple-600 via-pink-600 to-yellow-600">
           {text}
           <span className="animate-pulse">|</span>
