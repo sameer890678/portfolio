02d069e073c18453a9f9c752c3bc099afb74ead0
sameer_first <maliksameer890678@gmail.com>
2026-02-24 15:11:13 +0500

    redoing changes

--- app/page.tsx
+++ app/page.tsx
@@ -61,18 +61,28 @@ export default function Home() {
 </section>
 
 {/*Projetcs*/}
-     
+      <section className="py-20 px-4 bg-black-800">
+  <h2 className="text-3xl font-bold text-white text-center mb-10">Projects</h2>
+  <div className="grid grid-cols-1 md:grid-cols-2 gap-20 justify-items-center">
+    
+   {/* Tic Tac Toe Project */}
+<div className="p-6 rounded-lg text-white shadow-lg tictactoe-hover max-w-sm hover:scale-105 transition-transform duration-500">
+  <h3 className="text-2xl font-semibold mb-2">Tic Tac Toe Game</h3>
+  <p>
+    Built a 2-player Tic Tac Toe game in C++. Learned about arrays, loops, functions, and game logic implementation. Applied programming concepts to create an interactive console game.
+  </p>
+</div>
 
- <a
-      href="https://sameerportfolio598.vercel.app/Projects"
-      target="_blank"
-      className="flex items-center justify-center gap-2 px-4 py-3 rounded-full transform hover:scale-105 transition-all duration-300"
-      style={{
-        background: "linear-gradient(to right, black, purple)"
-      }}
-    >
-      <FaGithub size={17} /> Projects
-    </a>
+    {/* Army Management Project */}
+<div className="p-6 rounded-lg text-white shadow-lg army-hover max-w-sm hover:scale-105 transition-transform duration-500">
+  <h3 className="text-2xl font-semibold mb-2">Army Management System</h3>
+  <p>
+    Developed a console-based Army Management System in C++. Managed soldiers, weapons, and resources using data structures and file handling. Learned object-oriented programming concepts and organized complex data efficiently.
+  </p>
+</div>
+
+  </div>
+</section>
 
 {/* Links Section */}
 <section className="py-20 px-4 bg-gray-800 text-center">
