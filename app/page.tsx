188b79e3bbcd4f745ff95ff1b8bb5c0124329e03
sameer_first <maliksameer890678@gmail.com>
2026-07-02 16:10:05 +0500

    adding hover effect

--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
 <section className="py-20 px-4 bg-black text-center">
 <Link
   href="/Projects"
-  className="px-6 py-3 rounded-full text-white font-semibold hover:scale-105 transition-all duration-300"
+  className="px-6 py-3 rounded-full text-white font-semibold transform hover:scale-105 transition-all duration-300"
   style={{
     background: "linear-gradient(to right, purple, blue)"
   }}
